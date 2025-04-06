bb859d42ed740628f65ad48984d6f33b4e7b657b
Kate Cai <jc10236@nyu.edu>
2025-04-06 16:19:06 -0400

    Added more feature components, and changed their scales and positions accordingly

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="bg-background font-aclonica flex items-center justify-center min-h-screen">
       <div className="w-[1194px] h-[834px] bg-white border-4 border-foreground shadow-xl rounded-xl p-6 flex flex-col justify-start">
-        <h1 className="text-4xl font-bold text-center mb-4">Build Your OC!</h1>
+        {/* <h1 className="text-4xl font-bold text-center mb-4">Build Your OC!</h1> */}
         <OCBuilder />
       </div>
     </div>
